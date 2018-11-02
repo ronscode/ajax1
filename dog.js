7af2b46046281e3e934a49bf747ad12f1ebe68a1
Ron Cagle <ron@atlantashirt.com>
2018-11-02 16:13:22 -0400

    Fixed the button to reset togenerate another doggo.

--- dog.js
+++ dog.js
@@ -1,10 +1,14 @@
 function generateDog() {
-    axios.get('https://dog.ceo/api/breed/frise/images').then(function (response) {
+    axios.get('https://dog.ceo/api/breeds/image/random').then(function (data) {
         document.getElementById('imageContent').innerHTML = `
-        <img src="${response.data.message}"/>
+        <img src="${data.data.message}"/>
         `
       })
+      document.getElementById('dogGen').innerHTML = "Hecking Good Boye, Generate another!"
 }
+
+
+// This changes the text on the button once the button has been clicked. 
 
 function changeText() {
     document.getElementById('dogGen').innerHTML = "Generating Doggo";
